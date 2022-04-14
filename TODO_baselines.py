690ce3b8a3f96bee32a14291187e9006dda33bb3
nedimazar <nedimazar@gmail.com>
2022-04-14 20:48:41 +0200

    Fixed broken template code. Good night

--- TODO_baselines.py
+++ TODO_baselines.py
@@ -8,7 +8,9 @@ from model.data_loader import DataLoader
 
 # Each baseline returns predictions for the test data. The length and frequency baselines determine a threshold using the development data.
 
+
 def majority_baseline(train_sentences, train_labels, testinput, testlabels):
+    accuracy = None
     predictions = []
 
     # TODO: determine the majority class based on the training data
@@ -18,40 +20,35 @@ def majority_baseline(train_sentences, train_labels, testinput, testlabels):
     for instance in testinput:
         tokens = instance.split(" ")
         instance_predictions = [majority_class for t in tokens]
-        predictions.append(instance, instance_predictions)
+        predictions.append((instance, instance_predictions))
 
     # TODO: calculate accuracy for the test input
     # ...
     return accuracy, predictions
 
 
-
-
-
-if __name__ == '__main__':
-    train_path = "data/preprocessed/train"
-    dev_path = "data/preprocessed/dev"
-    test_path = "data/preprocessed/test"
+if __name__ == "__main__":
+    train_path = "data/preprocessed/train/"
+    dev_path = "data/preprocessed/val/"
+    test_path = "data/preprocessed/test/"
 
     # Note: this loads all instances into memory. If you work with bigger files in the future, use an iterator instead.
 
     with open(train_path + "sentences.txt") as sent_file:
         train_sentences = sent_file.readlines()
-
     with open(train_path + "labels.txt") as label_file:
         train_labels = label_file.readlines()
-
-
     with open(dev_path + "sentences.txt") as dev_file:
         dev_sentences = dev_file.readlines()
-
     with open(train_path + "labels.txt") as dev_label_file:
         dev_labels = dev_label_file.readlines()
     with open(test_path + "sentences.txt") as testfile:
         testinput = testfile.readlines()
+    with open(test_path + "labels.txt") as test_labelfile:
+        testlabels = test_labelfile.readlines()
 
-    with open("test_path + labels.txt") as test_labelfile:
-        testlabels = test_label_file.readlines()
-    majority_accuracy, majority_predictions = majority_baseline(train_sentences, train_labels, testinput)
+    majority_accuracy, majority_predictions = majority_baseline(
+        train_sentences, train_labels, testinput, testlabels
+    )
 
     # TODO: output the predictions in a suitable way so that you can evaluate them
