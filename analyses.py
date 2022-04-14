d49565e1c759558293f71cf74ac6e720a0f83bae
nedimazar <nedimazar@gmail.com>
2022-04-14 20:21:55 +0200

    Added TODOs @Martin

--- analyses.py
+++ analyses.py
@@ -4,10 +4,10 @@
 import spacy
 import numpy as np
 from tabulate import tabulate
+
 # from spacy import textacy
 from spacy import displacy
 from spacy.tokens.doc import Doc
-from preprocess import Preprocessor
 from collections import Counter
 import pandas as pd
 
@@ -137,9 +137,10 @@ def lemmas(doc: Doc):
                     print("\t\t", list(doc.sents)[i])
                     break
 
+
 def counter_to_relative(counter):
-    """ 
-        get relative frequencies of counter
+    """
+    get relative frequencies of counter
     """
     total_count = sum(counter.values())
     relative = {}
@@ -147,28 +148,28 @@ def counter_to_relative(counter):
         relative[key] = counter[key] / total_count
     return relative
 
+
 def part_of_the_speech(doc):
 
     pos_frequencies = Counter()
     pos_tags_dict = {}
     pos_tags = []
-    
+
     for sentence in doc.sents:
         # pos_tags = [token.pos_ for token in sentence]
         for token in sentence:
-            tokens = []    
+            tokens = []
             pos_tags.append(token.pos_)
             tokens.append(token.text)
-            
+
             if not token.pos_ in pos_tags_dict.keys():
                 pos_tags_dict[token.pos_] = Counter()
-            
+
             pos_tags_dict[token.pos_].update(tokens)
-            
-        
+
     pos_frequencies.update(pos_tags)
     relative_freq = counter_to_relative(pos_frequencies)
-    
+
     # print(pos_tags_dict)
     # print("most common verbs: \n",pos_tags_dict['VERB'].most_common(3),"\n")
     # print("less common verbs: \n",pos_tags_dict['VERB'].most_common()[-1],"\n")
@@ -176,44 +177,77 @@ def part_of_the_speech(doc):
     # print("relative freq:\n",relative_freq,"\n")
     # print("sum: \n",sum(pos_frequencies.values()))
 
-    return tabulate([[pos,pos_frequencies[pos],round(relative_freq[pos],2),pos_tags_dict[pos].most_common(3),pos_tags_dict[pos].most_common()[-1]] for pos in pos_tags_dict.keys()]
-        ,headers=["Finegrained POS-tag","Occurrences","Relative Tag Frequency (%)","3 Most frequent tokens","Infrequent token"])
+    return tabulate(
+        [
+            [
+                pos,
+                pos_frequencies[pos],
+                round(relative_freq[pos], 2),
+                pos_tags_dict[pos].most_common(3),
+                pos_tags_dict[pos].most_common()[-1],
+            ]
+            for pos in pos_tags_dict.keys()
+        ],
+        headers=[
+            "Finegrained POS-tag",
+            "Occurrences",
+            "Relative Tag Frequency (%)",
+            "3 Most frequent tokens",
+            "Infrequent token",
+        ],
+    )
+
 
 def extract_basic_statistics():
 
-    df = pd.read_csv('data/original/english/WikiNews_Train.tsv', sep='\t',header=None)
+    df = pd.read_csv("data/original/english/WikiNews_Train.tsv", sep="\t", header=None)
 
-    print(df.columns)
+    # print(df.columns)
 
-    print(df.iloc[1:5,10])
+    # print(df.iloc[1:5, 10])
 
-    #Number of instances labeled with 0:
-    print("The number of instances labeled with 0: ",len(df.loc[df.iloc[:,9] == 0]))
-    
-    #Number of instances labeled with 1:
-    print("The number of instances labeled with 1: ",len(df.loc[df.iloc[:,9] == 1]))    
+    # Number of instances labeled with 0:
+    print("The number of instances labeled with 0: ", len(df.loc[df.iloc[:, 9] == 0]))
 
-    #Min, max, median, mean, and stdev of the probabilistic label
-    print("Probabilistic Label:")    
-    print("\t max=",df.iloc[:,10].max())
-    print("\t min=",df.iloc[:,10].min())
-    print("\t median=",df.iloc[:,10].median())
-    print("\t mean=",df.iloc[:,10].mean())
-    print("\t stdev=",df.iloc[:,10].std())
+    # Number of instances labeled with 1:
+    print("\nThe number of instances labeled with 1: ", len(df.loc[df.iloc[:, 9] == 1]))
 
-    #Number of instances consisting of more than one token
-    mask=df.iloc[:,4].str.strip().str.split(' ').str.len()
-    print("Number of instances consisting of more than one token: ",len(df[mask > 1]))
-    
-    #Maximum number of tokens for an instance
-    max_length = df.iloc[:,4].str.strip().str.split(' ').str.len().max()
-    print("Maximum number of tokens for an instance: ", max_length, "\t instance: ")
-    print(df.iloc[:,4][df.iloc[:,4].str.strip().str.split(' ').str.len() > (max_length -1)])
+    # Min, max, median, mean, and stdev of the probabilistic label
+    print("\nProbabilistic Label:")
+    print("\t max=", df.iloc[:, 10].max())
+    print("\t min=", df.iloc[:, 10].min())
+    print("\t median=", df.iloc[:, 10].median())
+    print("\t mean=", round(df.iloc[:, 10].mean(), 2))
+    print("\t stdev=", round(df.iloc[:, 10].std(), 2))
+
+    # TODO
+    # We probably need to use the Spacy tokenizer so it will count punctuation
+    # Number of instances consisting of more than one token
+    mask = df.iloc[:, 1].str.strip().str.split(" ").str.len()
+    print(
+        "\nNumber of instances consisting of more than one token: ", len(df[mask > 1])
+    )
+
+    # TODO
+    # Same thing here with the tokenizer
+    # Maximum number of tokens for an instance
+    max_length = df.iloc[:, 1].str.strip().str.split(" ").str.len().max()
+    print("\nMaximum number of tokens for an instance: ", max_length, "\t instance: ")
+    print(
+        str(
+            df.iloc[:, 1][
+                df.iloc[:, 1].str.strip().str.split(" ").str.len() == max_length
+            ].iloc[:1]
+        )
+    )
+
 
 def main():
     nlp = spacy.load("en_core_web_sm")
 
-    with open("data/preprocessed/train/sentences.txt", "r",encoding="utf8") as text_file:
+    with open(
+        "data/preprocessed/train/sentences.txt", "r", encoding="utf8"
+    ) as text_file:
         text_data = text_file.read()
 
     # Replacing newline characters with a space, otherwise the newline character becomes a very common token
@@ -246,6 +280,7 @@ def main():
     # print("Word Classes - Most frequent POS tags: \n",part_of_the_speech(doc))
 
     extract_basic_statistics()
+
 
 if __name__ == "__main__":
     main()
